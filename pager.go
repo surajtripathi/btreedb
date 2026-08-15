b1d5dc8745fc9ab7fb94494abbdcc783541b213c
Suraj Triapthi <suraj99934@gmail.com>
2026-08-15 19:55:41 +0530

    feat: Pager for raw page I/O
    - readPage/writePage using ReadAt/WriteAt for random access (no O_APPEND)
    - allocatePage: sequential, gap-free page IDs, derived from file size on open
    - readPage rejects unallocated page IDs (off-by-one: counter is 'next free', not 'last used')
    - writePage rejects wrong-sized buffers
    - Explicit Sync()/Close(), no implicit fsync-per-write
    - Tests: write/read round trip, allocation sequencing, invalid page ID
      (including allocated-but-unwritten -> io.EOF), wrong buffer size,
      persistence across Pager instances verified via raw byte equality

--- pager.go
+++ pager.go
@@ -1,18 +1,75 @@
 package btreestore
 
-import "os"
+import (
+	"errors"
+	"os"
+)
 
-type Page struct {
-	file os.File
+type Pager struct {
+	file        *os.File
+	pageCounter uint64
 }
 
-const DIR = ""
-const FILE_PREFIX = ""
+var PageSizeMismatchError = errors.New("page size mismatch")
+var InvalidPageIDError = errors.New("page id can not be greater than the page counter")
 
-//func ReadPage(pageID string) ([]byte, error) {
-//
-//}
-//
-//func WritePage(bytes []byte, pageID string) error {
-//
-//}
+const DIR = "store"
+const FilePrefix = "btree"
+
+func newPager(path string) *Pager {
+	err := os.MkdirAll(DIR, 0o755)
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+	}
+	stat, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	fileSize := stat.Size()
+	return &Pager{
+		file:        file,
+		pageCounter: uint64(fileSize / pageSize),
+	}
+}
+
+func (p *Pager) readPage(pageID uint64) ([]byte, error) {
+	if pageID >= p.pageCounter {
+		return nil, InvalidPageIDError
+	}
+	pageOffset := pageID * pageSize
+	buff := make([]byte, pageSize)
+	_, err := p.file.ReadAt(buff, int64(pageOffset))
+	if err != nil {
+		return nil, err
+	}
+	return buff, nil
+}
+
+func (p *Pager) writePage(bytes []byte, pageID uint64) error {
+	if len(bytes) != pageSize {
+		return PageSizeMismatchError
+	}
+	_, err := p.file.WriteAt(bytes, int64(pageID*pageSize))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *Pager) allocatePage() uint64 {
+	pageID := p.pageCounter
+	p.pageCounter += 1
+	return pageID
+}
+
+func (p *Pager) Close() error {
+	return p.file.Close()
+}
+
+func (p *Pager) Sync() error {
+	return p.file.Sync()
+}
---
+++ pager_test.go
@@ -0,0 +1,191 @@
+package btreestore
+
+import (
+	"errors"
+	"io"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestPagerWriteAndRead(t *testing.T) {
+	path := filepath.Join(t.TempDir(), FilePrefix+".btree")
+	pager := newPager(path)
+	node := newLeafNode()
+	ok, err := node.insert("key1", "value1")
+	if err != nil || ok != true {
+		t.Errorf("error inserting key: %v, ok: %v", err, ok)
+	}
+	encode, err := node.encode()
+	if err != nil {
+		t.Errorf("error encoding node: %v", err)
+	}
+	pageID := pager.allocatePage()
+	err = pager.writePage(encode, pageID)
+	if err != nil {
+		t.Errorf("error writing page: %v", err)
+	}
+
+	buff, err := pager.readPage(pageID)
+	if err != nil {
+		t.Errorf("error reading page: %v", err)
+	}
+
+	decodedNode, err := decode(buff)
+	if err != nil {
+		t.Errorf("error decoding node: %v", err)
+	}
+	index, ok := decodedNode.search("key1")
+	if ok != true {
+		t.Errorf("error searching key1: %v", ok)
+	}
+	if index != 0 {
+		t.Errorf("error searching key1, expected index 0 but found: %v", index)
+	}
+	if decodedNode.kv[index].value != "value1" {
+		t.Errorf("error searching key1, expected value1 but found: %v", index)
+	}
+}
+
+func TestAllocatePageSequencing(t *testing.T) {
+	path := filepath.Join(t.TempDir(), FilePrefix+".btree")
+	pager := newPager(path)
+	pageID1 := pager.allocatePage()
+	pageID2 := pager.allocatePage()
+	pageID3 := pager.allocatePage()
+	pageID4 := pager.allocatePage()
+
+	if pageID1 != 0 || pageID2 != 1 || pageID3 != 2 || pageID4 != 3 {
+		t.Errorf("Sequencing data is not correct it should be 0, 1, 2, 3 but found %d, %d, %d, %d", pageID1, pageID2, pageID3, pageID4)
+	}
+
+}
+
+func TestReadPageInvalidPageID(t *testing.T) {
+	path := filepath.Join(t.TempDir(), FilePrefix+".btree")
+	pager := newPager(path)
+	_, err := pager.readPage(0)
+	if !errors.Is(err, InvalidPageIDError) {
+		t.Errorf("should have thrown error as page id is invalid %d", 0)
+	}
+	pager.allocatePage()
+	_, err = pager.readPage(1)
+	if !errors.Is(err, InvalidPageIDError) {
+		t.Errorf("should have thrown error as page id is invalid %d", 0)
+	}
+	// page allocated but not written and trying to read empty file
+	_, err = pager.readPage(0)
+	if !errors.Is(err, io.EOF) {
+		t.Errorf("should have thrown io.EOF as page id is invalid %d", 0)
+	}
+}
+
+func TestWritePageWithWrongBuffer(t *testing.T) {
+	path := filepath.Join(t.TempDir(), FilePrefix+".btree")
+	pager := newPager(path)
+	pageID1 := pager.allocatePage()
+	buff := make([]byte, 100)
+	err := pager.writePage(buff, pageID1)
+	if !errors.Is(err, PageSizeMismatchError) {
+		t.Errorf("should have thrown error as wrong size page trying to save, page size %d, expected size %d", len(buff), pageSize)
+	}
+}
+
+func TestPagerPersistedAcrossMultipleInstance(t *testing.T) {
+	node1 := newLeafNode()
+	node2 := newLeafNode()
+	node3 := newLeafNode()
+
+	_, err := node1.insert("key1", "value1")
+	if err != nil {
+		t.Errorf("error inserting key: %v", err)
+	}
+	_, err = node2.insert("key2", "value2")
+	if err != nil {
+		t.Errorf("error inserting key: %v", err)
+	}
+	_, err = node3.insert("key3", "value3")
+	if err != nil {
+		t.Errorf("error inserting key: %v", err)
+	}
+	path := filepath.Join(t.TempDir(), FilePrefix+".btree")
+
+	pagerInstance1 := newPager(path)
+
+	pageID1 := pagerInstance1.allocatePage()
+	encode1, err := node1.encode()
+	if err != nil {
+		t.Errorf("error encoding node: %v", err)
+	}
+	err = pagerInstance1.writePage(encode1, pageID1)
+	if err != nil {
+		t.Errorf("error writing page: %v", err)
+	}
+
+	pageID2 := pagerInstance1.allocatePage()
+	encode2, err := node2.encode()
+	if err != nil {
+		t.Errorf("error encoding node: %v", err)
+	}
+	err = pagerInstance1.writePage(encode2, pageID2)
+	if err != nil {
+		t.Errorf("error writing page: %v", err)
+	}
+
+	pageID3 := pagerInstance1.allocatePage()
+	encode3, err := node3.encode()
+	if err != nil {
+		t.Errorf("error encoding node: %v", err)
+	}
+	err = pagerInstance1.writePage(encode3, pageID3)
+	if err != nil {
+		t.Errorf("error writing page: %v", err)
+	}
+	err = pagerInstance1.Sync()
+	if err != nil {
+		t.Errorf("error syncing page: %v", err)
+	}
+
+	err = pagerInstance1.Close()
+	if err != nil {
+		t.Errorf("error closing page: %v", err)
+	}
+
+	pagerInstance2 := newPager(path)
+	page1Encoded, err := pagerInstance2.readPage(pageID1)
+	if err != nil {
+		t.Errorf("error reading page: %v", err)
+	}
+
+	if !reflect.DeepEqual(page1Encoded, encode1) {
+		t.Errorf("error reading page, expected %v but found %v", encode1, page1Encoded)
+	}
+
+	page2Encoded, err := pagerInstance2.readPage(pageID2)
+	if err != nil {
+		t.Errorf("error reading page: %v", err)
+	}
+
+	if !reflect.DeepEqual(page2Encoded, encode2) {
+		t.Errorf("error reading page, expected %v but found %v", encode2, page2Encoded)
+	}
+
+	page3Encoded, err := pagerInstance2.readPage(pageID3)
+	if err != nil {
+		t.Errorf("error reading page: %v", err)
+	}
+
+	if !reflect.DeepEqual(page3Encoded, encode3) {
+		t.Errorf("error reading page, expected %v but found %v", encode3, page3Encoded)
+	}
+
+	if pagerInstance2.pageCounter != 3 {
+		t.Errorf("pagerInstance2.pageCounter should be 3 but found %d", pagerInstance2.pageCounter)
+	}
+
+	err = pagerInstance2.Close()
+	if err != nil {
+		t.Errorf("error closing page: %v", err)
+	}
+
+}
